7c919f4b5798e0cd8b037a9b733788715ba21a39
FarazArshad01 <faraz.arshad125@gmail.com>
2025-11-21 09:06:31 +0500

    Refactor react_agent_basic.py for improved readability and add BMI workflow notebook
    - Reformatted code in react_agent_basic.py for better readability by adjusting line lengths and adding whitespace.
    - Introduced a new Jupyter notebook (1.sequential_workflow.ipynb) to demonstrate a BMI calculation workflow using StateGraph.
    - Added a second Jupyter notebook (2.simple_llm_workflow.ipynb) to set up the environment and import necessary libraries.
    - Updated requirements.txt to include additional dependencies for the new workflows.

--- 1. Introduction/react_agent_basic.py
+++ 1. Introduction/react_agent_basic.py
@@ -11,13 +11,19 @@ llm = ChatOpenAI(model="gpt-4o", temperature=0.0, max_tokens=1000)
 
 search_tool = TavilySearchResults()
 
+
 @tool
-def get_system_time(format:str= "%Y-%m-%d %H:%M:%S") -> str:
+def get_system_time(format: str = "%Y-%m-%d %H:%M:%S") -> str:
     """Get the current system time."""
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+
 tools = [search_tool, get_system_time]
 
-agent = initialize_agent(tools=tools, llm=llm, agent="zero-shot-react-description", verbose=True)
+agent = initialize_agent(
+    tools=tools, llm=llm, agent="zero-shot-react-description", verbose=True
+)
 
-agent.invoke("When was SpaceX's last launch and how many days ago was that from this instant")
+agent.invoke(
+    "When was SpaceX's last launch and how many days ago was that from this instant"
+)
